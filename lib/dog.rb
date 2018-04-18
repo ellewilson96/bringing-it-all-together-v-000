f8b36c9a142f4a31e0f98dd53b82228f51414671
Elle Wilson <eleanorwilsonn@gmail.com>
2018-04-18 00:35:49 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,6 +72,14 @@ class Dog
        dog
      end
 
+     
+   def self.new_from_db(row)
+   id = row[0]
+   name =  row[1]
+   breed = row[2]
+   self.new(id, name, breed)
+   end
+
      def self.find_by_name(name)
    sql = <<-SQL
      SELECT *
@@ -94,12 +102,6 @@ class Dog
   end.first
   end
 
-    def self.new_from_db(row)
-    id = row[0]
-    name =  row[1]
-    breed = row[2]
-    self.new(id, name, breed)
-    end
 
     def update
       sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
