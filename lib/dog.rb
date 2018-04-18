b69e75bc9c8afe3e91f57cab2300bc20c210882f
Elle Wilson <eleanorwilsonn@gmail.com>
2018-04-18 00:37:23 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -82,27 +82,31 @@ class Dog
    self.new(id, name, breed)
    end
 
-     def self.find_by_name(name)
-   sql = <<-SQL
-     SELECT *
-     FROM dogs
-     WHERE name = ?
-     LIMIT 1
-   SQL
- end
+   def self.find_by_name(name)
+     sql = <<-SQL
+       SELECT *
+       FROM dogs
+       WHERE name = ?
+       LIMIT 1
+     SQL
+
+     DB[:conn].execute(sql,name).map do |row|
+       self.new_from_db(row)
+     end.first
+   end
 
    def self.find_by_id(id)
-  sql = <<-SQL
-    SELECT *
-    FROM dogs
-    WHERE id = ?
-    LIMIT 1
-  SQL
+     sql = <<-SQL
+       SELECT *
+       FROM dogs
+       WHERE id = ?
+       LIMIT 1
+     SQL
 
-  DB[:conn].execute(sql,id).map do |row|
-    self.new_from_db(row)
-  end.first
-  end
+     DB[:conn].execute(sql,id).map do |row|
+       self.new_from_db(row)
+     end.first
+   end
 
 
     def update
