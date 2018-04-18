0811a9d475537c3ab36bc7dfa7bbcd0d876385d3
Elle Wilson <eleanorwilsonn@gmail.com>
2018-04-18 00:34:31 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -57,7 +57,7 @@ class Dog
        end
        dog
      end
-     
+
      def self.find_by_name(name)
    sql = <<-SQL
      SELECT *
@@ -75,7 +75,7 @@ class Dog
   DB[:conn].execute(sql,id).map do |row|
     self.new_from_db(row)
   end.first
-end
+  end
 
     def self.new_from_db(row)
     id = row[0]
