daceae222085435ea643f92e7048c93979c7b232
Elle Wilson <eleanorwilsonn@gmail.com>
2018-04-18 00:36:12 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,7 +6,9 @@ class Dog
   attr_reader :id
 
     def initialize(id: nil, name:, breed:)
-     @id, @name, @breed, = id, name, breed
+      @id = id
+     @name = name
+     @breed = breed
     end
 
     def self.create_table
