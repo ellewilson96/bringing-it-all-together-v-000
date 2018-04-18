ca3739edb8f6602a5e182442dc5d8974609157eb
Elle Wilson <eleanorwilsonn@gmail.com>
2018-04-18 00:32:08 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,14 +9,36 @@ class Dog
      @id, @name, @breed, = id, name, breed
     end
 
+    def self.create_table
+      sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogss (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+      SQL
+
+      DB[:conn].execute(sql)
+    end
+
+    
+    def self.drop_table
+      sql = "DROP TABLE IF EXISTS dogs"
+      DB[:conn].execute(sql)
+    end
+
     def save
+      if self.id
+      self.update
+    else
       sql = <<-SQL
         INSERT INTO dogs (name, breed)
         VALUES (?, ?)
       SQL
-
       DB[:conn].execute(sql, self.name, self.breed)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+    self
     end
 
     def self.create(name, breed)
@@ -51,22 +73,8 @@ class Dog
   end
   end
 
-    def self.create_table
-      sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS dogss (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        breed TEXT
-      )
-      SQL
+  
 
-      DB[:conn].execute(sql)
-    end
-
-    def self.drop_table
-      sql = "DROP TABLE IF EXISTS dogs"
-      DB[:conn].execute(sql)
-    end
 
     def update
       sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
